f4a5fa106573ec4acf6625e8d1bd1ab2dce8f8a9
Harry <harrytheripper1812@hotmail.com.ar>
2025-07-29 12:13:22 -0300

    Prepopulate category slug.

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -125,6 +125,11 @@ STATICFILES_DIRS = [
     "djangoecommerce/static",
 ]
 
+# Media files configuration 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from djangoecommerce.settings import MEDIA_ROOT, MEDIA_URL
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home")
-]
+] + static(MEDIA_URL, document_root =MEDIA_ROOT)
